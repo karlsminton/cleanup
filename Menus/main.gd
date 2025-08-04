3b35e40f1a574f8b7392cb7ac9d0216b3d80e05e
Karl Minton <kminton@frontier.co.uk>
2025-08-04 21:05:46 +0100

    init

---
+++ Menus/main.gd
@@ -0,0 +1,11 @@
+extends Button
+
+
+func _on_pressed_start_button() -> void:
+	get_tree().change_scene_to_file("res://Scenes/Overworld.tscn")
+
+func _on_options_button_pressed() -> void:
+	pass # Replace with function body.
+
+func _on_quit_button_pressed() -> void:
+	get_tree().quit(0)
